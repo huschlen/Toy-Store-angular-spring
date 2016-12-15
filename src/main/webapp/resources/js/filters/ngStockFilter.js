26a2d9dedce37be7b637a800a820a737774e7d84
huschlen <huschlen@comcast.net>
2016-12-15 16:37:39 -0600

    Cleaning up

--- src/main/webapp/resources/js/filters/ngStockFilter.js
+++ src/main/webapp/resources/js/filters/ngStockFilter.js
@@ -3,18 +3,15 @@ angular
 	.filter('ngStockFilter', function() {
 
 		return function(listings, stockInfo) {
-
 			var filtered = [];
-
 			var min = stockInfo.min;
 			var max = stockInfo.max;
-
 			angular.forEach(listings, function(listing) {
 				if(listing.stock >= min && listing.stock <= max) {
 					
 					filtered.push(listing);
 				}
 			});
-				return filtered;
+			return filtered;
 		}
 	});
