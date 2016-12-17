e7606a729a6dd41d929da79c8e682ab7eb4bfbd3
huschlen <huschlen@comcast.net>
2016-12-16 18:42:02 -0600

    Commented out @GeneratedValue(strategy=GenerationType.AUTO)

--- src/main/java/com/naoko/inventory/entity/Toy.java
+++ src/main/java/com/naoko/inventory/entity/Toy.java
@@ -16,7 +16,7 @@ public class Toy implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	//@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="tid")
     private int tid;
 	@Column(name="name")
