f8c8caeab0fffff0ff79f7395eaa85e7f05e5317
huschlen <huschlen@comcast.net>
2016-12-21 16:40:16 -0600

    More test cases

--- src/test/java/com/naoko/inventory/service/ToyServiceTest.java
+++ src/test/java/com/naoko/inventory/service/ToyServiceTest.java
@@ -3,89 +3,120 @@ package com.naoko.inventory.service;
 import com.naoko.inventory.dao.IToyDAO;
 import com.naoko.inventory.entity.Toy;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import java.util.ArrayList;
-import java.util.Optional;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.boot.test.SpringApplicationConfiguration;(deprecated)
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;(Use WebMvcTest for testing controller)
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.junit.Before;
 import org.mockito.runners.MockitoJUnitRunner;
-import com.sun.jersey.api.client.ClientResponse;
+import static org.mockito.Mockito.*;
+import org.mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ToyServiceTest {
 
-	private Toy toy = new Toy();
+	@Mock
 	private static IToyService toyService;
 	@Mock
-    private IToyDAO toyDAO;
+    private IToyDAO toyDAO;;
 	
-	@Before
-	public void setup() {
-		this.toyService; = Mockito.mock(IToyService.class);
-	}
-
-    @Test
-	 public void testResponsesOnFind() {
-	 //simulate Person ID = 1 in the DB
-	 when(dao.findByPrimaryKey(1)).thenReturn(new Person(1,"John","Doe",null));
-	 
-	 //test response when ID exists
-	 Response found = service.find(1);
-	 assertEquals(200, found.getStatus());
-	 
-	 //test response when ID does not exist in DB
-	 Response notFound = service.find(999);
-	 assertEquals(404, notFound.getStatus());
-	 }
 	
 	@Test
 	public void testGetToyById() {
-		this.toy.setId(1);
-		this.toy.setName("Motoko");
-		this.toy.setCategory("Doll");
-		this.toy.setPrice(100);
-		this.toy.setDescription("Super cute");
-		this.toy.setStock(2);
+		Toy toy = new Toy();
+		toy.setTid(1);
+		toy.setName("Barbie Sara");
+		toy.setCategory("Doll");
+		toy.setPrice(100);
+		toy.setDescription("Super cute");
+		toy.setStock(2);
 		// expected toy
-		when(toyService.getToyById(1)).thenReturn(Optional.of(toy));
+		Mockito.when(toyService.getToyById(1)).thenReturn(toy);
 		// check value
 		assertEquals(toy, toyService.getToyById(1));
 		// verify interaction
-		verify.toyService.getToyById(1));
+		verify(toyService).getToyById(1);
 		
 	}
-	//@Test
-	/*public void testGetAllToys(){
-		toyService.getAllToys();
+	@Test
+	public void testGetAllToys(){
+		List<Toy> toys = new ArrayList<>();
+		Toy toy1 = new Toy();
+		toy1.setTid(1);
+		toy1.setName("Barbie Sara");
+		toy1.setCategory("Doll");
+		toy1.setPrice(100);
+		toy1.setDescription("Super cute");
+		toy1.setStock(2);
+		Toy toy2 = new Toy();
+		toy2.setTid(2);
+		toy2.setName("Barbie Dave");
+		toy2.setCategory("Doll");
+		toy2.setPrice(100);
+		toy2.setDescription("Super handsome");
+		toy2.setStock(1);
+		toys.add(toy1);
+		toys.add(toy2);
+
+		Mockito.when(toyService.getAllToys()).thenReturn(toys);
+		assertEquals(2, toyService.getAllToys().size());
+
 	}
-	//@Test
-	public void testAddToy(Toy toy){
-		toyService.addToy(toy);
+	@Test
+	public void testAddToy(){
+		// toy to create
+        Toy toy1 = new Toy();
+		toy1.setTid(1);
+		toy1.setName("Barbie Sara");
+		toy1.setCategory("Doll");
+		toy1.setPrice(100);
+		toy1.setDescription("Super cute");
+		toy1.setStock(2);
+                         
+        toyService.addToy(toy1);
+        Mockito.verify(toyService, Mockito.times(1)).addToy(toy1);
+    }
+	@Test
+	public void testUpdateToy() {
+		Toy toy1 = new Toy();
+		toy1.setTid(1);
+		toy1.setName("Barbie Sara");
+		toy1.setCategory("Doll");
+		toy1.setPrice(100);
+		toy1.setDescription("Super cute");
+		toy1.setStock(2);
+		Toy toyUpdated = new Toy();
+		toyUpdated.setTid(1);
+		toyUpdated.setName("Dave Stuffed Animal");
+		toyUpdated.setCategory("Stuffed Animal");
+		toyUpdated.setPrice(100);
+		toyUpdated.setDescription("Super handsome");
+		toyUpdated.setStock(1);
+		
+		Mockito.when(toyService.addToy(toy1)).thenReturn(true);
+		toyService.updateToy(toy1);
+		Mockito.when(toyService.getToyById(1)).thenReturn(toyUpdated);
+		
+		assertEquals(toyUpdated, toyService.getToyById(1));
+		verify(toyService).updateToy(toy1);
 	}
-	//@Test
-	public void testUpdateToy(Toy toy) {
-		toyService.updateToy(toy);
+	@Test
+	public void testDeleteToy() {
+		// toy to delete
+        Toy toy1 = new Toy();
+		toy1.setTid(1);
+		toy1.setName("Barbie Sara");
+		toy1.setCategory("Doll");
+		toy1.setPrice(100);
+		toy1.setDescription("Super cute");
+		toy1.setStock(2);
+                         
+        toyService.deleteToy(toy1.getTid());
+        Mockito.verify(toyService, Mockito.times(1)).deleteToy(toy1.getTid());
 	}
-	//@Test
-	public void testDeleteToy(int tid) {
-		toyService.deleteToy(tid);
-	}*/
 
 }
 
